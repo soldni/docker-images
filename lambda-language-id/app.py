dfc8e869e1ec21ab64a40602e00f5a076ff01ad4
Luca Soldaini <lucas@allenai.org>
2023-02-24 11:35:21 -0800

    first version of lambda lang id

---
+++ lambda-language-id/app.py
@@ -0,0 +1,22 @@
+import fasttext     # type: ignore
+
+
+# Load the FastText language identification model
+model = fasttext.load_model('lid.176.bin')
+
+
+# Define the lambda function to detect language of input text
+def detect_language(text: str) -> str:
+    return model.predict(text)[0][0].split('__')[-1]
+
+
+# Define the Lambda handler function
+def handler(event, context):
+    # Get the text input from the event
+    text = event['text']
+
+    # Detect the language of the text using FastText
+    language_code = detect_language(text)
+
+    # Return the language code as the output of the Lambda function
+    return {'language_code': language_code}
