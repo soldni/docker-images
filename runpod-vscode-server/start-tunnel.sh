21d78c10f7eb85589fa32092af5420101ab7e461
Luca Soldaini <lucas@allenai.org>
2024-09-14 22:05:36 -0400

    small fix

--- runpod-vscode-server/start-tunnel.sh
+++ runpod-vscode-server/start-tunnel.sh
@@ -1,27 +1,16 @@
 #! /usr/bin/env bash
 
-# Configuration
-PERSISTENT_DIR="/workspace"
-CLI_DATA_DIR="${PERSISTENT_DIR}/.vscode-server/cli-data"
-SERVER_DATA_DIR="${PERSISTENT_DIR}/.vscode-server/server-data"
-EXTENSIONS_DIR="${PERSISTENT_DIR}/.vscode-server/extensions"
-IDENTITY_PROVIDER="github"
-
-# Check if directory $PERSISTENT_DIR exists; if not; error out
-if [ ! -d "${PERSISTENT_DIR}" ]; then
-    echo "Directory ${PERSISTENT_DIR} does not exist."
+# Check if GITHUB_TOKEN is set, otherwise raise an error
+if [ -z "${GITHUB_TOKEN}" ]; then
+    echo "GITHUB_TOKEN is not set. Please set the GITHUB_TOKEN environment variable."
     exit 1
 fi
 
-# Check if the user is logged in by checking if "code tunnel user show" reuturns "not logged in"
-if [ "$(code tunnel user show)" == "not logged in" ]; then
-    code tunnel user login --cli-data-dir "${CLI_DATA_DIR}" --provider "${IDENTITY_PROVIDER}"
-fi
+# Setup Git to use the provided GitHub token
+gh auth setup-git
+
+# Log into GitHub to us tunnel
+code tunnel user login --provider "github" --access-token "${GITHUB_TOKEN}"
 
 # Start the tunnel
-code tunnel \
-    --server-data-dir "${SERVER_DATA_DIR}" \
-    --extensions-dir "${EXTENSIONS_DIR}" \
-    --name "${RUNPOD_POD_HOSTNAME:0:20}" \
-    --accept-server-license-terms \
-    --cli-data-dir "${CLI_DATA_DIR}"
+code tunnel --name "${RUNPOD_POD_HOSTNAME:0:20}" --accept-server-license-terms
